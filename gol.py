af67d43bd92df0ae85e2f50f05569e416f901fd3
EmpressT 1Love <99760212+twardally@users.noreply.github.com>
2022-09-13 00:04:33 +0000

    started gol

---
+++ gol.py
@@ -0,0 +1,42 @@
+#gol.py
+#Tanya Wardally
+#CSCI 77800 Fall 2022
+#collaborators:
+#consulted:
+
+rows
+cols
+
+board[][]  = new char [rows][cols]
+
+for i < in board.length:
+  for j < in board[0].length:
+    board[i][j] = '-'
+
+    return board
+
+
+
+for i < board.length:
+  for j <  board[0].length
+      print (board[i][j], " ")
+
+  print "\n"
+
+int rrows
+int ccols
+char val
+
+board[rrows][ccols] = val
+
+
+int count = 0
+
+
+
+
+
+
+
+
+  
