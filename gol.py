419cc9d270ab5fb0d52cf2daabbaf990a02f8270
EmpressT 1Love <99760212+twardally@users.noreply.github.com>
2022-09-22 00:36:33 +0000

    updated gol.py

--- gol.py
+++ gol.py
@@ -4,39 +4,68 @@
 #collaborators:
 #consulted:
 
-rows
-cols
+import random, copy
 
-board[][]  = new char [rows][cols]
+rows = 25
+cols = 25
 
-for i < in board.length:
-  for j < in board[0].length:
-    board[i][j] = '-'
+Alive = 'X'
+Dead = '-'
 
-    return board
+board = {}
 
+for x in range (rows):
+  for y in range (cols):
+    if random.randint(0, 1) == 0:
+      board[(x,y)] = Alive
+    else:
+      board[(x,y)] = Dead
+    
 
-
-for i < board.length:
-  for j <  board[0].length
-      print (board[i][j], " ")
-
-  print "\n"
-
-int rrows
-int ccols
-char val
-
-board[rrows][ccols] = val
+while True:
+  print('\n')
+  cells = copy.deepcopy(board)
 
 
-int count = 0
+  for x in range (rows):
+        for y in range (cols):    
+            print(cells[(x,y)], end = " ")
+  print()
 
 
+for y in range (cols):
+  for x in range (rows):
+    left = (x-1) % rows
+    right = (x+1) % rows
+    above = (y-1) % cols
+    below = (y+1) % cols
 
 
+    numNeighbors = 0
+    if cells[(left, above)] == Alive:
+      numNeighbors += 1  # Top-left neighbor is alive.
+    if cells[(x, above)] == Alive:
+      numNeighbors += 1  # Top neighbor is alive.
+    if cells[(right, above)] == Alive:
+      numNeighbors += 1  # Top-right neighbor is alive.
+    if cells[(left, y)] == Alive:
+      numNeighbors += 1  # Left neighbor is alive.
+    if cells[(right, y)] == Alive:
+      numNeighbors += 1  # Right neighbor is alive.
+    if cells[(left, below)] == Alive:
+      numNeighbors += 1  # Bottom-left neighbor is alive.
+    if cells[(x, below)] == Alive:
+      numNeighbors += 1  # Bottom neighbor is alive.
+    if cells[(right, below)] == Alive:
+      numNeighbors += 1  # Bottom-right neighbor is alive.
 
 
-
-
-  
+    if cells[(x, y)] == Alive and (numNeighbors == 2 or numNeighbors == 3):
+                   # Living cells with 2 or 3 neighbors stay alive:
+      board[(x, y)] = Alive
+    elif cells[(x, y)] == DEAD and numNeighbors == 3:
+                # Dead cells with 3 neighbors become alive:
+      board[(x, y)] = Alive
+    else:
+                # Everything else dies or stays dead:
+      board[(x, y)] = Dead
