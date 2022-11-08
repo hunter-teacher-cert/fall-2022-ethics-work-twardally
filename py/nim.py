56298c29d6add34a2e0615a6f57baa54c200245c
twardally <99760212+twardally@users.noreply.github.com>
2022-11-08 13:31:41 -0500

    Create nim.py

---
+++ py/nim.py
@@ -0,0 +1,36 @@
+#nim.py
+#Tanya Wardally
+#CSCI 77800 Fall 2022
+#collaborators:
+#consulted:
+
+stones = 12
+while stones >= 0:
+  
+    user_num = input('Pick a number from 1 to 3: ')
+    
+    stones = stones - int(user_num)
+    print ("The number of objects is now: ",  stones)
+while int(user_num)<1 or int(user_num)>3: 
+
+    print ("There are," + stones + " stones left")
+
+if stones == 0:
+  print ("You Won!!")
+  #break
+
+if stones <= 3:
+  com_num = stones
+
+print ("The computer took, ",  com_num,  " stones")
+
+stones = stones - com_num
+
+print ("The number of objects is now: ",  stones)
+
+if stones == 0:
+  print ("you Won!!")
+  #break
+
+
+  
